3613de2199008431a1ddb0bba73c70f2663969a6
ZakiyaA <zakiyaalfughi@gmail.com>
2023-03-21 20:33:58 -0400

    update messaning area height

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -4,13 +4,7 @@ const Message = () => {
   return (
     <div className="message-body">
     <p>message1</p>
-    <p>message1</p>
-    <p>message1</p>
-    <p>message1</p>
-    <p>message1</p>
-    <p>message1</p>
-    <p>message1</p>
-    <p>message1</p>
+   
    
 </div>
   )
